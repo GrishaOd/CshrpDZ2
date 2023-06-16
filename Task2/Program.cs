bd5a5c63963ae7f58c2955bdc17d0e77f9637484
Grisha <odnokonnygrisha@gmail.com>
2023-06-16 13:22:42 +0300

    feat: all tasks done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,6 +4,10 @@
 Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
 
+if (a < 0)
+{
+    a = a * -1;
+}
 if (a < 100 && a > 0)
 {
     Console.WriteLine("Здесь нет третьей цифры");
